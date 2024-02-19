a0bc43da66fa203a5d19265c61b34cd244cd041d
yibai100 <1250349299@qq.com>
2024-02-19 21:20:48 +0800

    签到统计(BitMap)

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -11,13 +11,20 @@ import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -38,6 +45,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Autowired
     public RedisTemplate redisTemplate;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public Result getCode(String phone, HttpSession session) {
@@ -98,5 +108,70 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomNumbers(6));
         save(user);
         return user;
+    }
+
+    @Override
+    public Result sign() {
+        //获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        //获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String keySuffix= now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key=USER_SIGN_KEY+userId+keySuffix;
+        //获取今天是本月第几天
+        int dayOfMonth=now.getDayOfMonth();
+        //写入redis setBit key offset 1
+        stringRedisTemplate.opsForValue().setBit(key,dayOfMonth-1,true);
+
+        return Result.ok();
+    }
+
+
+    @Override
+    public Result signCount() {
+
+        //获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        //获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String keySuffix= now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key=USER_SIGN_KEY+userId+keySuffix;
+        //获取今天是本月第几天
+        int dayOfMonth=now.getDayOfMonth();
+
+
+        //获取本月截至今天所有签到记录，返回十进制 BITFIELD sign:5:202203 GET u14 0
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key,
+                BitFieldSubCommands.create()
+                        .get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+
+        //没有签到
+        if(result == null || result.isEmpty()){
+            return Result.ok(0);
+        }
+        Long num=result.get(0);
+        if(num==null || num==0){
+            return Result.ok(0);
+        }
+
+        int count=0;
+        //循环遍历
+        while(true){
+            if((num & 1) == 0){
+                //为0，结束
+                break;
+            }else{
+                //不为0，计数器+1
+                count++;
+            }
+
+            //右移赋值
+            num >>>= 1;
+        }
+        return Result.ok(count);
     }
 }
